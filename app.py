78f09ef76f1f468ddc4caff4c2675c06446b23d6
kishi_ryoya <bjkzmkdr7@outlook.jp>
2025-12-28 08:18:56 +0900

    app.pyを修正

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 from company_blog import app
+from company_blog import db
+from company_blog.models import User
+
+def init_db_if_needed(app):
+    with app.app_context():
+        db.create_all()
+
+        # 管理ユーザーがいなければ作成
+        if not User.query.filter_by(email="admin_user@test.com").first():
+            admin = User(
+                email="admin_user@test.com",
+                username="Admin User",
+                password="123",
+                administrator="1"
+            )
+            db.session.add(admin)
+            db.session.commit()
 
 if __name__=='__main__':
     app.run()
+
+init_db_if_needed(app)
